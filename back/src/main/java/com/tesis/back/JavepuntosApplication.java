bf8ddcbbb954eb80aec58d041fda8ec88a43d6c5
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2023-08-28 19:08:42 -0500

    Creación de proyecto Spring
    Creacion de proyecto en spring con dependencias:
    - Spring web
    - Spring Boot Dev Tools
    - JDBC API
    - Spring Data JPA

---
+++ back/src/main/java/com/tesis/back/JavepuntosApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavepuntosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavepuntosApplication.class, args);
+	}
+
+}
