42799943079bd95d1ad169697a6844cee65c3a76
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2023-11-05 16:09:23 -0500

    Metodo para traer articulos de una promocion dada

--- Back-lb4/back-lb4/src/controllers/articulos-promociones.controller.ts
+++ Back-lb4/back-lb4/src/controllers/articulos-promociones.controller.ts
@@ -20,13 +20,15 @@ Articulos,
 Articulopromocion,
 Promociones,
 } from '../models';
-import {ArticulosRepository} from '../repositories';
+import {ArticulopromocionRepository, ArticulosRepository} from '../repositories';
 import {authenticate} from '@loopback/authentication';
+import {prototype} from 'events';
 
 @authenticate('jwt')
 export class ArticulosPromocionesController {
   constructor(
     @repository(ArticulosRepository) protected articulosRepository: ArticulosRepository,
+    @repository(ArticulopromocionRepository) protected articulosPromocionRepository: ArticulopromocionRepository
   ) { }
 
   @get('/articulos/{id}/promociones', {
@@ -109,4 +111,42 @@ export class ArticulosPromocionesController {
   ): Promise<Count> {
     return this.articulosRepository.promociones(id).delete(where);
   }
+
+// busca articulos de una promo
+  @get('/promociones/{id}/articulos', {
+    responses: {
+      '200': {
+        description: 'Array of Articulo instances associated with the specified Promocion',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Articulos, { includeRelations: true }),
+            },
+          },
+        },
+      },
+    },
+  })
+  async findArticulosByPromocion(@param.path.number('id') id: number): Promise<Articulos[]> {
+    // Buscar los ids de los artículos asociados a la promoción
+    const articuloPromociones: Articulopromocion[] = await this.articulosPromocionRepository.find({
+      where: { id_promocion: id },
+    });
+
+    // Extraer los ids de los artículos asociados a la promoción
+    const articuloIds: number[] = articuloPromociones
+      .map(ap => ap.id_articulo)
+      .filter(id => id !== undefined) as number[];
+
+    // Buscar los artículos utilizando los ids obtenidos
+    const articulos: Articulos[] = await this.articulosRepository.find({
+      where: { id_articulo: { inq: articuloIds } },
+    });
+
+    return articulos;
+
+  }
+
 }
+
