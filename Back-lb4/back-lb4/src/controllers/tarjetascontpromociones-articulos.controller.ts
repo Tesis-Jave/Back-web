f9ef44c0bd55484d8d439f2406773726d4d8c276
German Andrés Velasco Bossa <germana-velasco@javeriana.edu.co>
2023-11-08 00:06:08 -0500

    recomendacion de productos

--- Back-lb4/back-lb4/src/controllers/tarjetascontpromociones-articulos.controller.ts
+++ Back-lb4/back-lb4/src/controllers/tarjetascontpromociones-articulos.controller.ts
@@ -19,15 +19,23 @@ import {
 Tarjetascontpromociones,
 Productosredimidos,
 Articulos,
+Tarjetas
 } from '../models';
-import {TarjetascontpromocionesRepository,ArticulosRepository,ProductosredimidosRepository} from '../repositories';
+import {TarjetascontpromocionesRepository,ArticulosRepository,ProductosredimidosRepository,TarjetasRepository} from '../repositories';
 import {authenticate} from '@loopback/authentication';
+import * as fs from 'fs';
+import { FPGrowth } from 'node-fpgrowth';
 
-// Define el tipo PromocionWithArticulos
-interface TransaccionProductos {
-  id_transaccion: number|undefined;
-  id_cliente:number|undefined;
-  productos: string[];
+// Define el tipo Item
+interface Transaccion{
+  id_cliente:number,
+  articulos:number[]
+}
+
+// Define una interfaz para Itemset
+interface Itemset<T> {
+  items: T;
+  support: number;
 }
 
 @authenticate('jwt')
@@ -36,9 +44,11 @@ export class TarjetascontpromocionesArticulosController {
     @repository(TarjetascontpromocionesRepository) protected tarjetascontpromocionesRepository: TarjetascontpromocionesRepository,
     @repository(ArticulosRepository) protected articulosRepository: ArticulosRepository,
     @repository(ProductosredimidosRepository) protected productosRedimidosRepo: ProductosredimidosRepository,
+    @repository(TarjetasRepository) protected tarjetasRepository: TarjetasRepository,
   ) { }
 
-  @get('/tarjetascontpromociones/all-articulos', {
+  // Endpoint para obtener recomendaciones basadas en filtrado colaborativo
+  @get('/tarjetascontpromociones/recomendaciones/{id}', {
     responses: {
       '200': {
         description: 'Array of Articulos associated with the specified Tarjetascontpromociones',
@@ -50,34 +60,116 @@ export class TarjetascontpromocionesArticulosController {
       },
     },
   })
-  async findAllArticulos(): Promise<(TransaccionProductos)[]> {
-    const tarjetascontpromociones: Tarjetascontpromociones[] = await this.tarjetascontpromocionesRepository.find();
-    var transacciones:TransaccionProductos[]=[]
-    for ( var i =0;i<tarjetascontpromociones.length;i++){
-      var transaccionesAux = await this.productosRedimidosRepo.find({
-        where:{
-          id_transaccion:tarjetascontpromociones[i].id_transaccion,
+  async findCollaborativeFilteringRecommendations(@param.path.number('id') id: number): Promise<Articulos[] | null> {
+    // Obtén las transacciones asociadas con el cliente actual
+    const transaccionesClienteActual = await this.tarjetascontpromocionesRepository.find({
+      where: {
+        id_tarjeta: id, // ID del cliente para el que se generan recomendaciones
+      },
+    });
+
+    // Extrae los IDs de transacción de las transacciones encontradas
+    const comprasClienteActualIds = transaccionesClienteActual.map((transaccion) => transaccion.id_transaccion);
+
+    // Obtén las compras anteriores del cliente actual (el cliente para el que se generan recomendaciones)
+    const comprasClienteActual = await this.tarjetascontpromocionesRepository.articulosredimidos(id).find();
+
+    // Obtén los productos comprados por el cliente actual
+    const productosCompradosPorClienteActual = comprasClienteActual
+      .map((compra) => compra.id_articulo)
+      .filter((producto) => producto !== undefined) as number[];
+
+    // Obtén la matriz de usuarios y compras
+    const userPurchaseMatrix: { userId: number; products: number[] }[] = await this.buildUserPurchaseMatrix();
+
+    // Supongamos que tienes una función llamada "getCollaborativeFilteringRecommendations" que toma el ID del cliente y sus compras como entrada
+    const collaborativeRecommendations = this.getCollaborativeFilteringRecommendations(id, productosCompradosPorClienteActual, userPurchaseMatrix);
+
+    // Si obtuviste recomendaciones, busca los detalles de los productos recomendados
+    if (collaborativeRecommendations.length > 0) {
+      const recommendedProductIds = collaborativeRecommendations.map((recommendation) => recommendation);
+      const recommendedProducts = await this.articulosRepository.find({
+        where: {
+          id_articulo: { inq: recommendedProductIds },
         },
-      })
-      if(transaccionesAux.length>0){
-        
-        const idTransaccion = tarjetascontpromociones[i].id_transaccion;
-        const idCliente = tarjetascontpromociones[i].id_tarjeta;
-        const productos : string[]=[]
+      });
+      return recommendedProducts;
+    } else {
+      console.log(`No se encontraron recomendaciones para el cliente ${id}`);
+      return null;
+    }
+  }
 
-        for(var a = 0; a<transaccionesAux.length;a++){
-          productos.push((await this.articulosRepository.findById(transaccionesAux[a].id_articulo)).descripcion)
-        }
+  async buildUserPurchaseMatrix(): Promise<{ userId: number; products: number[] }[]> {
+    const tarjetascontpromociones = await this.tarjetascontpromocionesRepository.find();
 
-        transacciones.push({id_transaccion:idTransaccion,id_cliente:idCliente,productos:productos})
+    // Construye la matriz de usuarios y compras a partir de los datos reales en la base de datos
+    const userPurchaseMatrix: { userId: number; products: number[] }[] = [];
+
+    for (const tc of tarjetascontpromociones) {
+      const userId = tc.id_tarjeta;
+
+      const productosredimidos = await this.productosRedimidosRepo.find({
+        where: {
+          id_transaccion: tc.id_transaccion,
+        },
+      });
+
+      const productIds = productosredimidos
+        .map((producto) => producto.id_articulo)
+        .filter((id) => typeof id === 'number') as number[];
+
+      // Busca si el usuario ya existe en la matriz
+      const userIndex = userPurchaseMatrix.findIndex((user) => user.userId === userId);
+
+      if (userIndex !== -1) {
+        userPurchaseMatrix[userIndex].products.push(...productIds);
+      } else {
+        userPurchaseMatrix.push({ userId, products: productIds });
       }
     }
-    if (!tarjetascontpromociones) {
-      throw new Error('Tarjetascontpromociones not found');
-    }
-    // Obtener los IDs de Articulos de la propiedad articulosredimidos
-    return transacciones;
+
+    return userPurchaseMatrix;
   }
+
+
+
+  getCollaborativeFilteringRecommendations(
+    userId: number,
+    userPurchases: number[],
+    userPurchaseMatrix: { userId: number; products: number[] }[]
+  ): number[] {
+    const similarUsers: { userId: number; similarity: number }[] = [];
+  
+    // Calcular la similitud entre el usuario actual y otros usuarios
+    for (const user of userPurchaseMatrix) {
+      if (user.userId !== userId) {
+        const commonProducts = userPurchases.filter((product) => user.products.includes(product));
+        const similarity = commonProducts.length / Math.sqrt(userPurchases.length * user.products.length);
+        similarUsers.push({ userId: user.userId, similarity });
+      }
+    }
+  
+    // Ordenar los usuarios por similitud en orden descendente
+    similarUsers.sort((a, b) => b.similarity - a.similarity);
+  
+    // Obtener las recomendaciones de productos basadas en usuarios similares
+    const recommendations: number[] = [];
+    const mostSimilarUser = userPurchaseMatrix.find((u) => u.userId === similarUsers[0].userId);
+    if (mostSimilarUser) {
+      for (const product of mostSimilarUser.products) {
+        if (!userPurchases.includes(product)) {
+          recommendations.push(product);
+        }
+      }
+    }
+  
+    return recommendations;
+  }
+  
+  
+  
+
 
   @get('/tarjetascontpromociones/{id}/articulos', {
     responses: {
